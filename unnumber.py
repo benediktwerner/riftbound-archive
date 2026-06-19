e142bc2b20d48a6c3cbabab869c575321554f572
Benedikt Werner <1benediktwerner@gmail.com>
2026-06-19 16:44:07 +0200

    Improvements to unnumbering

--- unnumber.py
+++ unnumber.py
@@ -21,41 +21,27 @@ if not os.path.isfile(txtfile):
 with open(txtfile) as f:
     text = f.read()
 
+PATTERNS = [
+    r"\d{3}\.? ",
+    r"\d{3}\.\d+\.? ",
+    r"\d{3}\.\d+\.[a-z]\.? ",
+    r"\d{3}\.\d+\.[a-z]\.\d+\.? ",
+    r"\d{3}\.\d+\.[a-z]\.\d+\.[a-z]\.? ",
+    r"\d{3}\.\d+\.[a-z]\.\d+\.[a-z]\.\d+\.? ",
+]
+
 text = re.sub(r"\x0c", "\n", text)
 text = re.sub(r"^\s+", "", text, flags=re.MULTILINE)
 text = re.sub(r"\n([^0-9])", r" \1", text)
 
-PATTERNS = [
-    r"\d\d+\. ",
-    r"\d+\.\d+\.? ",
-    r"\d+\.\d+\.[a-z]\.? ",
-    r"\d+\.\d+\.[a-z]\.\d+\.? ",
-    r"\d+\.\d+\.[a-z]\.\d+\.[a-z]\.? ",
-    r"\d+\.\d+\.[a-z]\.\d+\.[a-z]\.\d+\.? ",
-]
-
-lines = []
-for line in text.splitlines():
-    if " " not in line:
-        lines.append(line)
-        continue
-
-    num, rest = line.split(maxsplit=1)
-    if any(re.fullmatch(r"(?<![Ss]ee )" + p, num + " ") for p in PATTERNS):
-        if num in rest:
-            lines.append(num + " " + re.sub(" " + num, "\n" + num, rest))
-            continue
-
-    lines.append(line)
-
-text = "\n".join(lines) + "\n"
-
+for p in PATTERNS:
+    text = re.sub(r"\n" + p[:-1] + "-" + p[:-1], r" XXX-XXX", text)
 
 for i, p in enumerate(PATTERNS):
     text = re.sub("^" + p, "  " * i + "- ", text, flags=re.MULTILINE)
-
-for p in reversed(PATTERNS):
     text = re.sub(" " + p, " XXX. ", text)
+    text = re.sub(" " + p[:-1] + "\n", " XXX.\n", text)
+    text = re.sub(" " + p[:-1] + ",", " XXX,", text)
 
 with open(outfile, "w") as of:
     of.write(text)
